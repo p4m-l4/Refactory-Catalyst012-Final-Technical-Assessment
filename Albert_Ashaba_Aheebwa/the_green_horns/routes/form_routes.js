1d52fd97ab770a7450993866b87aa7d3b46b4c02
TrippleA-Ashaba <trippleaunit@hotmail.com>
2022-11-25 12:59:21 +0300

    added image uploads

--- Albert_Ashaba_Aheebwa/the_green_horns/routes/form_routes.js
+++ Albert_Ashaba_Aheebwa/the_green_horns/routes/form_routes.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const Patient = require("../models/Patient");
+const multer = require("multer");
 
-router.get("/", (req, res) => {
-	res.render("form", { messages: req.flash("info") });
+const Student = require("../models/Student");
+
+// image upload
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "public/uploads");
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	},
 });
 
-router.post("/", async (req, res) => {
+// instantiate variable upload to store multer functionality to upload image
+const upload = multer({ storage: storage });
+
+router.get("/", (req, res) => {
+	// res.render("form", { messages: req.flash("info") });
+	res.render("form");
+});
+
+router.post("/", upload.single("avatar"), async (req, res) => {
 	console.log(req.body);
 	try {
-		let patient = new Patient(req.body);
-		await patient.save();
-		req.flash("info", "Patient registered successfully");
+		let student = new Student(req.body);
+		student.avatar = req.file.path;
+		await student.save();
+		// req.flash("info", "Student registered successfully");
 		res.redirect("/");
 	} catch (err) {
 		console.log(err);
--- Albert_Ashaba_Aheebwa/the_green_horns/routes/table_routes.js
+++ Albert_Ashaba_Aheebwa/the_green_horns/routes/table_routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const Patient = require("../models/Patient");
+// const Patient = require("../models/Patient");
 
 router.get("/", async (req, res) => {
 	try {
-		let patients = await Patient.find();
-		console.log(patients);
-		res.render("table", { patients });
+		// let patients = await Patient.find();
+		// console.log(patients);
+		res.render("table");
 	} catch (err) {
 		console.log(err);
 		res.status(404).send(err);
